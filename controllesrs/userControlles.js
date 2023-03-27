4e31f85c39dbb5f415789ecd6293c636700ecd71
Azer <imranovazer@gmail.com>
2023-03-27 15:47:32 +0400

    little changes

--- controllesrs/userControlles.js
+++ controllesrs/userControlles.js
@@ -57,6 +57,7 @@ exports.login = async (req, res) => {
       res.status(200).json({
         status: "Success",
         data: {
+          id: user._id,
           name: user.name,
           email: user.email,
           token,
